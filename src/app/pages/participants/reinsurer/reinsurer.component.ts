e8f4d5b3b6b96bdb7234988e76c0eb85f14a3261
alekyaragipally <35074678+alekyaragipally@users.noreply.github.com>
2021-03-01 13:36:38 +0530

    Broker and Reinsurer changes

--- src/app/pages/participants/reinsurer/reinsurer.component.ts
+++ src/app/pages/participants/reinsurer/reinsurer.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-reinsurer',
   templateUrl: './reinsurer.component.html',
   styleUrls: ['./reinsurer.component.scss']
 })
-export class ReinsurerComponent implements OnInit {
+export class ReinsurerComponent implements OnInit, AfterViewInit {
 
-  reinsurerForm: FormGroup
+  reinsurerForm: FormGroup;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  columns: string[] = ['reinsurerName', 'reinsurerLocation', 'reinsurerCorporationLocation', 'federalID', 'action'];
+  dataSource;
+  showUpdateRIBtn: string;
 
   constructor(
     private fb: FormBuilder
@@ -21,6 +26,25 @@ export class ReinsurerComponent implements OnInit {
       'reinsurerCorporationLocation': [''],
       'federalID': ['']
     });
+    this.dataSource = [
+      { reinsurerName: 'Test', reinsurerLocation: 'Test Loc', reinsurerCorporationLocation: 'Test', federalID: 'Test', action: '' },
+      { reinsurerName: 'Test', reinsurerLocation: 'Test Loc', reinsurerCorporationLocation: 'Test', federalID: 'Test', action: '' },
+      { reinsurerName: 'Test', reinsurerLocation: 'Test Loc', reinsurerCorporationLocation: 'Test', federalID: 'Test', action: '' },
+      { reinsurerName: 'Test', reinsurerLocation: 'Test Loc', reinsurerCorporationLocation: 'Test', federalID: 'Test', action: '' }
+    ];
+    this.showUpdateRIBtn = 'Add';
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  editReInsurer(){
+    this.showUpdateRIBtn = 'Update';
+  }
+
+  updateReinsurer(){
+    this.showUpdateRIBtn = 'Add';
   }
 
 }
